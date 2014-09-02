fc4d9e71ca4b9301dadf526897b8c3f5c378bdb2
mogemimi <mogemimi@enginetrouble.net>
2014-09-03 02:59:05 +0900

    Update vignette shader

--- Content/Shaders/Vignette/PixelShader.glsl
+++ Content/Shaders/Vignette/PixelShader.glsl
@@ -12,17 +12,48 @@ uniform sampler2D Texture;
 
 out vec4 FragColor;
 
+///@param radius [0.0, 1.0], default: 0.5
+///@param softness [0.0, 1.0], default: 0.05
+float CircleVignette(vec2 textureCoord, float radius, float softness)
+{
+	// NOTE:
+	// [0, 1] => [-0.5, +0.5]
+	vec2 position = In.TextureCoord.xy - vec2(0.5);
+	float len = length(position);
+	float vignette = smoothstep(radius, radius - softness, len);
+	return vignette;
+}
+
+///@param radius [0.0, 1.0], default: 0.5
+///@param softness [0.0, 1.0], default: 0.05
+///@param scale [0.0, 1.0] default: 1.0
+float ScaledCircleVignette(vec2 textureCoord, float radius, float softness, vec2 scale)
+{
+	vec2 position = (In.TextureCoord.xy - vec2(0.5)) * scale;
+	float len = length(position);
+	float vignette = smoothstep(radius, radius - softness, len);
+	return vignette;
+}
+
 void main()
 {
 	float dummy = (0.0 * (RenderTargetPixelSize.x / RenderTargetPixelSize.y));
 	
-	float radius = 3.7;
-	float darkness = 0.7;
+	const float radius = 0.518;
+	const float softness = 0.014;
+	
+	//float vignette = CircleVignette(In.TextureCoord.xy, radius, softness);
+	
+	float vignette = ScaledCircleVignette(In.TextureCoord.xy, radius, softness, vec2(1.0, 0.3))
+		* ScaledCircleVignette(In.TextureCoord.xy, radius, softness, vec2(0.5, 0.9));
 	
 	vec4 color = texture(Texture, In.TextureCoord.xy);
-	vec2 inTex = In.TextureCoord.xy - vec2(0.5, 0.5);
-	float vignette = 1.0 - dot(inTex, inTex);
-	color.rgb *= clamp(pow(vignette, radius) + darkness, 0.0, 1.0);
+	
+	// Multiply blending
+	//color.rgb *= vignette;
+	
+	// Linear Burn blending
+	color.rgb = max(color.rgb + vignette - 1.0, 0.0);
 	
 	FragColor = dummy + color;
 }
